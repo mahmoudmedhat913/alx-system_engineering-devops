0d1b1a9dda86d19f40fa47f6bf56797d7d98be65
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-03-25 06:40:00 -0700

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """export data in csv format"""
 
 if __name__ == "__main__":
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     name = user.json().get('username')
     todos = requests.get('https://jsonplaceholder.typicode.com/todos')
 
-    filename = username + '.csv'
+    filename = userId + '.csv'
     with open(filename, mode='w') as f:
         writer = csv.writer(f, delimiter=',', quotechar='"',
                             quoting=csv.QUOTE_ALL, lineterminator='\n')
