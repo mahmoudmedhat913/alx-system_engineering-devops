fd1c4694f41327a20c53805a0124f51758e52c19
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-04-09 09:16:30 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
 
     sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
                             .format(subreddit),
-                            headers = {"User-Agent": "My-User-Agent",
+                            headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
 
     if sub_info.status_code >= 300:
