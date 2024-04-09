d93f06c504ccd4188a0bb4075b6fbc5b7f01ca39
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-04-09 09:10:35 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,8 +6,6 @@ def number_of_subscribers(subreddit):
     """function that queries the Reddit API
     and returns the number of subscribers"""
     import requests
-    if subreddit is None or type(subreddit) is not str:
-        return 0
 
     base_url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': '0x16-api_advanced_project'}
