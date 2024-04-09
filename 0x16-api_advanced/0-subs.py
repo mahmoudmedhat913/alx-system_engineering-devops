5bc2691cf8a4c15e8ef6025885c03f391f2e75aa
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-04-09 03:29:25 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """module for number_of_subscribers"""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """function that queries the Reddit API
-    and returns the number of subscribers"""
-    import requests
-
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    if subreddit is None or type(subreddit) is not str:
         return 0
 
-    return sub_info.json().get("data").get("subscribers")
+    base_url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': '0x16-api_advanced'}
+    r = requests.get(base_url, headers=headers, allow_redirects=False).json()
+
+    if r.get('data') is None:
+        return 0
+
+    subscribers = r.get('data').get('subscribers')
+    return subscribers
