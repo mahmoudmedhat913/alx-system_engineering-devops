cdb4f3ad1177462aba984d5f761d6be7cef27bb7
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-04-09 09:15:27 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,12 @@ def number_of_subscribers(subreddit):
     and returns the number of subscribers"""
     import requests
 
-    base_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': '0x16-api_advanced_project'}
-    r = requests.get(base_url, headers=headers, allow_redirects=False)
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers = {"User-Agent": "My-User-Agent",
+                            allow_redirects=False)
 
-    if r.status_code >= 300:
+    if sub_info.status_code >= 300:
         return 0
     
-     return r.json().get('data').get('subscribers')
+     return sub_info.json().get("data").get("subscribers")
